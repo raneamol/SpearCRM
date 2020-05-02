61d6ce2db4f277a0736080fac5885b201b7a897f
VedantPimpley <vedantpimpley7@gmail.com>
2020-05-02 18:45:40 +0530

    Corrected data flow in ActivityTracker and activity components

--- src/components/subcomponents/PastActivity.js
+++ src/components/subcomponents/PastActivity.js
@@ -1,18 +1,16 @@
 import React from 'react'
 import '../styles/PrettyList.css'
 
-export default function NextSteps(props) {
+export default function PastActivity(props) {
   return(
     <> 
-      <h2> Next Steps </h2>
+      <h2> Past Activity</h2>
       <div className="pretty-list">
-        
         <ul className="experiences">
           {props.activitiesList.map( (element, i) => {
             return (
               <li className="blue" key={i}>
-                <input className="largerCheckbox" type="checkbox" />
-                <div className="where"> {element.title} </div>
+                <div className="where"> {element.elapsed ? "&#10006;" : "" } {element.title} </div>
                 <div className="when"> {element.date} </div>
                 <p className="description"> {element.body} </p>
              </li>
