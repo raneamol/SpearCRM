379b2ac7ffbff57aa9a8a4ce79bb16ebd3e22d58
VedantPimpley <vedantpimpley7@gmail.com>
2020-05-02 18:43:25 +0530

    Corrected data flow in ActivityTracker and to activity components

--- src/components/subcomponents/ActivityTracker.js
+++ src/components/subcomponents/ActivityTracker.js
@@ -3,6 +3,7 @@ import '../styles/ActivityTracker.css'
 import ManualLogger from './ManualLogger.js'
 import PrettyList from './PrettyList.js'
 import NextSteps from './NextSteps'
+import PastActivity from './PastActivity'
 import NewOrderDialogBox from './NewOrderDialogBox'
 
 export default function ActivityTracker(props) {
@@ -10,17 +11,16 @@ export default function ActivityTracker(props) {
   const [activityTitle, setActivityTitle] = useState("");
   const [activityBody, setActivityBody] = useState("");
   const [activityDate, setActivityDate] = useState(new Date().toJSON().slice(0,10));
-  const [activitiesList, setActivitiesList] = useState("");
+  const [activitiesList, setActivitiesList] = useState([]);
 
 //   [{"_id": "5eaade1967f5adbdd24460a7", "title": "Finalize Amol's order", "body": "eh", "date": “2020-02-04T15:08:56.000Z”, "activity_type": "future", "user_id": "5ea58fbc63e50fc607cf6a10", "elapsed": 0}, 
 //   {"_id": "5eaade2467f5adbdd24460a8", "title": "Finalize Amol's order", "body": "eh", "date": “2020-02-04T15:08:56.000Z”, "activity_type": "past", "user_id": "5ea58fbc63e50fc607cf6a10", "elapsed": 0}]
 
   useEffect( () => {
     fetch(`/main/display_account_orders/${props._id}`).then(response => {
-      return null;
+      response.json().then( data => setActivitiesList(data) )
     });
   }, []);
-
 
   const handleActivityType = (event, newActivityType) => {
     if (newActivityType !== null) {
@@ -67,8 +67,7 @@ export default function ActivityTracker(props) {
   return(
     <div className="activity-tracker-container">
       <h2 style={{ textAlign: "center"}}> Activity Tracker</h2>
-      {console.log(props.orders)}
-      <NewOrderDialogBox account_id={props.account_id}/> 
+      <NewOrderDialogBox account_id={props._id}/> 
       <EmailAutomator />
       <ManualLogger 
         draftType={activityType} 
@@ -81,8 +80,8 @@ export default function ActivityTracker(props) {
         handleChangeInDate={handleChangeInDate}
         postNewActivity = {postNewActivity}
       />
-      <NextSteps />
-      <PastActivity />
+      <NextSteps activitiesList={activitiesList.filter(activity => activity["activity_type"] == "future")} />
+      <PastActivity activitiesList={activitiesList.filter(activity => activity["activity_type"] == "past")} />
     </div>
   ); 
 }
